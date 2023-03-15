4da641a3961adffda69f7aa3398a167b849ee6fa
Sebastian GA <isebastian.ga@gmail.com>
2023-03-14 22:23:32 -0500

    add functionality to each button of customization

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-const background = document.getElementsByClassName("img-background")[0];
-const neck = document.getElementsByClassName("img-neck")[0];
-const nose = document.getElementsByClassName("img-nose")[0];
-const hair = document.getElementsByClassName("img-hair")[0];
-const ears = document.getElementsByClassName("img-ears")[0];
-const accessories = document.getElementsByClassName("img-accessories")[0];
-const eyes = document.getElementsByClassName("img-eyes")[0];
-const mouth = document.getElementsByClassName("img-mouth")[0];
-const legs = document.getElementsByClassName("img-legs")[0];
+const img_sections = ["background", "neck", "hair", "ears", "accessories", "eyes", "mouth", "leg"];
 
-// Background
-const buttons_background = document.getElementById("customize-background").children[1].childNodes;
-buttons_background.forEach((button) => {
-    button.addEventListener("click", () => {
-        background.setAttribute("src", button.children[0].getAttribute("src"));
+// Go trough each btns-section and add functionality to each button
+img_sections.forEach((section) => {
+    const btns_container = document.getElementById("customize-" + section).children[1].childNodes;
+    btns_container.forEach((button) => {
+        button.addEventListener("click", () => {
+            const img_section = document.getElementById("img-" + section);
+            img_section.setAttribute("src", button.children[0].getAttribute("src"));
+        });
     });
 });
