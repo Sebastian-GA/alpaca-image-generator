ed95b0c4714c3a78df14c7602d233228e3e581f5
Sebastian GA <isebastian.ga@gmail.com>
2023-03-14 23:04:44 -0500

    no accessories option done

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ img_sections.forEach((section) => {
     btns_container.forEach((button) => {
         button.addEventListener("click", () => {
             const img_section = document.getElementById("img-" + section);
-            img_section.setAttribute("src", button.children[0].getAttribute("src"));
+            if (button.classList.contains("custom-option-dont")) {
+                img_section.setAttribute("display", "none");
+                img_section.style.display = "none";
+            } else {
+                img_section.style.display = "unset";
+                img_section.setAttribute("src", button.children[0].getAttribute("src"));
+            }
         });
     });
 });
